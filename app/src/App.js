64bc2f5751249e37b80e4ee9aa3adc7fd766b289
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-03-19 15:41:44 -0300

    Intentando hacer el login.

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,35 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import Loger from "./components/Loger/Loger";
+import Cards from './components/Cards/Cards';
 import Register from "./components/Register/Register";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [access, setAccess] = useState(false)
+  const username = 'nahuel@gmail.com';
+  const password = 'nahuel1234';
+
+  const navigate = useNavigate();
+
+  const login = userData => {
+    if (userData.username === username &&
+        userData.password === password) {
+      setAccess(true);
+      navigate('/home');
+    }
+  }
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Loger/>}/>
-         {/* <Route path="/login" element={<Loger />} /> */}
+        <Route
+          path="/"
+          element={<Loger login={login} />}
+        />
+        <Route path='/home' element={<Cards />} />
         <Route path="/sign-up" element={<Register />} />
       </Routes>
     </div>
   );
 }
-
-export default App;
