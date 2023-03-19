8ae742028101d54fc52db29725d51d300b8243bf
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-03-19 13:17:28 -0300

    Sacando los estilos a un archivo css separado, la imagen no se posiciona correctamente.

--- app/src/components/Loger/Loger.jsx
+++ app/src/components/Loger/Loger.jsx
@@ -1,95 +1,75 @@
-import React, { Component, useState } from "react";
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React, { /*Component,*/ useState } from "react";
+import { Link } from 'react-router-dom';
+import Styles from './Loger.module.css';
 
-const Door = styled.div`
-display: flex;
-justify-content: center;
-flex-direction: column;
-align-items: center;
-padding: 2rem;
-`
+export default function Loger () {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
 
-const Login = styled.form`
-border: 2px solid grey;
-border-radius: 20px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-width: 20%;
-padding-bottom: 1rem;
-`
+  const handleInputChange = (event) => {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-const Data = styled.input`
-font-family: Arial, Helvetica, sans-serif;
-font-size: 1rem;
-margin-left: 3rem;
-margin-right: 3rem;
-margin-top: 2.5rem;
-
-`
-const Click = styled.button`
-font-family: Arial, Helvetica, sans-serif;
-font-size: 1rem;
-border: none;
-border-radius: 5px;
-padding: 0.2rem;
-background-color: #aaaaef;
-margin-left: 4rem;
-margin-right: 4rem;
-margin-top: 2rem;
-
-cursor: pointer;
-`
-const R = styled.span`
-font-family: Arial, Helvetica, sans-serif;
-font-size: 0.8rem;
-margin-bottom: 2.5rem;
-`
-const Portada = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: space-around;
-align-items: center;
-margin: 3rem;
-`
-
-const Logo = styled.img`
-width: 70%;
-margin: 0 auto;
-`
-
-const Loger = ()=> {
-const [input, setInput] = useState(
-    {
-     email: '',
-     password: ''   
-    }
-)
-
-const handleChange = (event) => {
-const {name, value} = event.target
-setInput({...input, [name]: value})
-}
-
-const handleSubmit = () => {
-console.log(input)
-alert('Registrado')
-}
+  const handleSubmit = () => {
+    alert('Registrado');
+  };
 
     return(
-        <Door>
-        <Portada>
-            <Logo src="https://sacudete.icbf.gov.co//themes/sacudetethemev2/assets/img/agendate.png" alt="Logo" />
-        </Portada>   
-        <Login onSubmit={handleSubmit}>
-            <Data name="email" type="email" placeholder="Enter email" onChange={(e)=>handleChange(e)}/>
-            <Data name="password" type="password" placeholder="Enter password" onChange={(e)=>handleChange(e)}/>
-            <Click type="submit">Login</Click>
-            <Link to='/sign-up'><R>Registrarse</R></Link>
-        </Login>
-        </Door>
-    )
+        <section>
+          {/*
+          <div className={Styles.frontPage}>
+            <img
+              className={Styles.logo}
+              src="https://sacudete.icbf.gov.co//themes/sacudetethemev2/assets/img/agendate.png"
+              alt="img" />
+          </div>
+           */}
+          <div className={Styles.formBox}>
+            <form onSubmit={handleSubmit}>
+              <div className={Styles.formGroup}>
+                <h2>Bienvenido</h2>
+                <div className={Styles.inputBox}>
+                  <ion-icon name="mail-outline"></ion-icon>
+                  <label htmlFor='email'>Email: </label>
+                  <input
+                    id='email'
+                    name="email"
+                    type="email"
+                    value={userData.email}
+                    placeholder="Ingresa tu email"
+                    onChange={handleInputChange}
+                    required />
+                </div>
+              </div>
+                <div className={Styles.formGroup}>
+                  <div className={Styles.inputBox}>
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                    <label htmlFor='password'>Contraseña: </label>
+                    <input
+                      id='password'
+                      name="password"
+                      type="password"
+                      value={userData.password}
+                      placeholder="Ingresa tu contraseña"
+                      onChange={handleInputChange}
+                      required />
+                  </div>
+                </div>
+              <button
+                className={Styles.button}
+                type="submit" >
+                Ingresar
+              </button>
+              <div className={Styles.register}>
+                <p>No tenes cuenta <Link to='/sign-up'>Registrate</Link></p>
+              </div>
+            </form>
+          </div>
+        </section>
+    );
 }
-
-export default Loger
