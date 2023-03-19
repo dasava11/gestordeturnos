ebb4e729513373a06174a4a4bca3315ff9fe0cdc
Nahuel <lescanoagustin10nahuel@gmail.com>
2023-03-19 20:54:03 -0300

    Agregando funcionalidad a SearchBar.

--- app/src/components/SearchBar/SearchBar.jsx
+++ app/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import Styles from './SearchBar.module.css';
 
 export default function SearchBar({ buscarPaciente }) {
+  const [paciente, setPaciente] = useState('');
+
+  const handleChange = event => {
+    setPaciente(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setPaciente('');
+  };
+
   return (
-    <div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className={Styles.container}>
+        <input
+          value={paciente}
+          onChange={handleChange}
+          type='search'
+          className={Styles.input}
+          placeholder='Buscar dni paciente'
+        />
+        <button
+          type='submit'
+          className={Styles.button}
+          onClick={() => buscarPaciente(paciente.dni)} >
+          Agregar
+        </button>
+      </div>
+    </form>
   )
 }
